8259751e0bba7f810afcdb0cd3b5266a909c6998
xchs <xchs@gmx-topmail.de>
2012-05-07 00:06:53 +0200

    Test commit.

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php phpinfo(); ?>
+
+<?php
+// phpinfo(INFO_GENERAL);
+// phpinfo(INFO_CREDITS);
+// phpinfo(INFO_CONFIGURATION);
+// phpinfo(INFO_MODULES);
+// phpinfo(INFO_ENVIRONMENT);
+// phpinfo(INFO_VARIABLES);
+// phpinfo(INFO_LICENSE);
+// phpinfo(INFO_ALL);
+?>
